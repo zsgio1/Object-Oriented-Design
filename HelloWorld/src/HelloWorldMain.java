c753081f3abb968669511106a9dd900a8921133f
Zachary Giofkou <zsgio1@student.monash.edu>
2018-02-27 15:30:38 +1100

    locate directory

---
+++ HelloWorld/src/HelloWorldMain.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author zacha
+ *
+ */
+public class HelloWorldMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+	}
+
+}
